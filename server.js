820e431f5b6cdf7c3ecb74a2bf629838a1e4488d
remy29 <arthur.d.remy@gmail.com>
2020-11-28 19:55:03 +0000

    general style improvements

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ const widgetsRoutes = require("./routes/widgets");
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+
 app.use( express.static( "public" ) ); // had to put this here to get images to load with ejs
 
 // Note: mount other resources here, using the same pattern above
