7be55e6e7ae3a7b8162d688a785f6528803b1f05
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-28 09:29:47 -0800

    Simplify login route: remove db lookup

--- routes/users.js
+++ routes/users.js
@@ -9,17 +9,12 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
+  // Login form is a single input: user_id
+  // which submits to GET /users/login/[user_id]
+  router.get('/login/:id', (req, res) => {
+    req.session.user_id = req.params.id;
+    res.redirect('/');
+  })
+
   return router;
 };
