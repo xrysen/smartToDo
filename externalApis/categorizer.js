6dc6a6d887aa686af00993fb58573372467e974b
Sean Oyler <xrysen@gmail.com>
2020-11-29 22:56:35 +0000

    Adds requestAll that requests data from all API's and returns an array of values

--- externalApis/categorizer.js
+++ externalApis/categorizer.js
@@ -1,5 +1,3 @@
-
-
 const request = require("request-promise-native");
 // TODO 1. Refine Woflman Alphadog API call, this doesn't always return valuable info
 const isTextBookOrMovieTitle = (text) => {
@@ -8,8 +6,8 @@ const isTextBookOrMovieTitle = (text) => {
   return request(`https://api.wolframalpha.com/v2/query?input=${text}&format=plaintext&output=json&appid=${process.env.API_KEY_WOLFRAMALPHA}`)
   .then(res => {
     res = JSON.parse(res);
-    console.log('wolfram alpha response res= --------------\n', res);
-    console.log('wolfram alpha response array res.queryresult.assumptions.values= --------------\n', res.queryresult.assumptions.values);
+    // console.log('wolfram alpha response res= --------------\n', res);
+    // console.log('wolfram alpha response array res.queryresult.assumptions.values= --------------\n', res.queryresult.assumptions.values);
     const wolframCatOptionsArr = res.queryresult.assumptions.values;
     for (const obj of wolframCatOptionsArr) {
       if (obj.name === 'Movie' || 'MovieClass') {
@@ -19,7 +17,7 @@ const isTextBookOrMovieTitle = (text) => {
         return 'read'
       }
     }
-    return '4';
+    return null;
   })
 };
 
@@ -28,49 +26,82 @@ const requestBookByTitle = (title) => {
   return request(`https://www.googleapis.com/books/v1/volumes?q=${title}`)
   .then(res => {
     res = JSON.parse(res);
-    // console.log('-----googleapis/books response res.items[0].volumeInfo.title= --------------\n', res.items[0].volumeInfo.title);
-    // console.log('-----googleapis/books response res.items[1].volumeInfo.title= --------------\n', res.items[1].volumeInfo.title);
-    const googleBooksOptionsArr = res.items;
-    for (const obj of googleBooksOptionsArr) {
-      if (obj.volumeInfo.title === title) {
-        return 'read';
-      }
+    console.log('-----googleapis/books response res.items[0].volumeInfo.title= --------------\n', res.items[0].volumeInfo.title);
+    //console.log('-----googleapis/books response res.items[1].volumeInfo.title= --------------\n', res.items[1].volumeInfo.title);
+    //const googleBooksOptionsArr = res.items;
+    // for (const obj of googleBooksOptionsArr) {
+    //   if (obj.volumeInfo.title === title) {
+    //     return 'read';
+    //   }
+    // }
+    if (res.items[0].volumeInfo.title.toLowerCase().includes(title.toLowerCase())) {
+      return 'read';
+    } else {
+      return null;
     }
-    return;
     // return res.items[0].volumeInfo.title;
   });
 };
 
+
 const requestMovieByTitle = (title) => {
-  return request(`http://www.omdbapi.com/?s=${title}&apikey=${process.env.API_KEY_OMDB}`)
+  return request(`http://www.omdbapi.com/?s=${title}&page=1&apikey=${process.env.API_KEY_OMDB}`)
   .then(res => {
     res = JSON.parse(res);
-    // console.log('omdbapi response res= --------------\n', res);
+    //console.log('omdbapi response res= --------------\n', res);
     const omdbOptionsArr = res.Search;
-    for (const obj of omdbOptionsArr) {
-      if (obj.Title === title) {
-        return 'watch';
-      }
+    // for (const obj of omdbOptionsArr) {
+    //   if (obj.Title === title) {
+    //     return 'watch';
+    //   }
+    // }
+    if (res.Response === "False") {
+      return null
+    } else {
+      return 'watch';
     }
-    return;
   })
 };
+
 
 // TODO 2. Implement location-finder function
 const requestRestaurantByNameAndLoc = (name, location) => {
   console.log('--------called requestRestaurantByNameAndLoc()-----');
 
   return request({
-    url: `https://api.yelp.com/v3/businesses/search?term=${name}&location=${location}`,
+    url: `https://api.yelp.com/v3/businesses/search?term=${name}&location=${location}&categories=restaurants&limit=1`,
     headers: { Authorization: `Bearer ${process.env.API_KEY_YELP}` }
   })
   .then(res => {
     res = JSON.parse(res);
     console.log('yelp response res= --------------\n', res);
-    return 'eat';
+    if (!res.businesses.length || !res.businesses[0].name.toLowerCase().includes(name.toLowerCase())) {
+      return null;
+    }
+    else {
+      return 'eat';
+    }
     // return res.businesses[0].name;
   });
 };
+
+const requestAll = (name, location) => {
+  Promise.all([
+    //isTextBookOrMovieTitle(name),
+    requestRestaurantByNameAndLoc(name, location),
+    requestMovieByTitle(name),
+    requestBookByTitle(name)
+  ])
+  .then(res => {
+    return Promise.all(res.map(data => data));
+  })
+  .then(data => {
+    console.log(data);
+    //return data;
+  });
+};
+
+requestAll("IT","vancouver");
 
 // 3. Set up call-sequence and promise-handling inside categorizeTask
 module.exports = (taskText) => {
