4136c32280a89f070987a00c8274603097ac034c
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-28 23:22:29 -0800

    WIP API call using promises writes data to DB
    - add switch case for categorizer return
    TODO: data is not correct on write, API calls need work

--- externalApis/categorizer.js
+++ externalApis/categorizer.js
@@ -1,47 +1,59 @@
 
 
 const request = require("request-promise-native");
-
-// const requestBookByTitle = (title) => {
-//   return request(`https://www.googleapis.com/books/v1/volumes?q=${title}`)
-//   .then(res => {
-//     res = JSON.parse(res);
-//     return res.items[0].volumeInfo.title;
-//   });
-// };
-
-// const searchForMovie = (title) => {
-//   return request(`http://www.omdbapi.com/?s=${title}&apikey=f3fb7868`)
-//   .then(res => {
-//     res = JSON.parse(res);
-//     return res.Search[0].Title;
-//   })
-// };
-
-// TODO::
-// 1. Implement Woflman Alphadog API
+// TODO 1. Refine Woflman Alphadog API call, this doesn't always return valuable info
 const isTextBookOrMovieTitle = (text) => {
   console.log('--------called isbookormovie()-----');
 
   return request(`https://api.wolframalpha.com/v2/query?input=${text}&format=plaintext&output=json&appid=${process.env.API_KEY_WOLFRAMALPHA}`)
   .then(res => {
     res = JSON.parse(res);
-    console.log('wolfram alpha response array res --------------\n', res);
+    console.log('wolfram alpha response array res.queryresult.assumptions.values --------------\n', res.queryresult.assumptions.values);
+    const wolframCatOptionsArr = res.queryresult.assumptions.values;
+    for (const obj of wolframCatOptionsArr) {
+      if (obj.name === 'Movie' || 'MovieClass') {
+        return 'watch';
+      }
+      if (obj.name === 'Book' || 'BookClass') {
+        return 'read'
+      }
+    }
+
     return '4';
   })
 };
 
-// 2. Implement location-finder function
-const searchForRestaurant = (name, location) => {
+// TODO 2. Implement bookFinder and movieFinder API calls
+const requestBookByTitle = (text) => {
+  return request(`https://www.googleapis.com/books/v1/volumes?q=${text}`)
+  .then(res => {
+    res = JSON.parse(res);
+    return 'read';
+    // return res.items[0].volumeInfo.title;
+  });
+};
+
+const searchForMovie = (text) => {
+  return request(`http://www.omdbapi.com/?s=${text}&apikey=${process.env.API_KEY_OMDB}`)
+  .then(res => {
+    res = JSON.parse(res);
+    return 'watch';
+    // return res.Search[0].Title;
+  })
+};
+
+// TODO 2. Implement location-finder function
+const searchForRestaurant = (text, location) => {
   return request({
-    url: `https://api.yelp.com/v3/businesses/search?term=${name}&location=${location}`,
+    url: `https://api.yelp.com/v3/businesses/search?term=${text}&location=${location}`,
     headers: {
       Authorization: `Bearer ${process.env.API_KEY_YELP}`
     }
   })
   .then(res => {
     res = JSON.parse(res);
-    return res.businesses[0].name;
+    return 'eat';
+    // return res.businesses[0].name;
   });
 };
 
@@ -51,11 +63,27 @@ module.exports = (taskText) => {
   // const category = Math.floor(Math.random()*4);
 
   console.log('task sent to categorizer is:::', taskText);
-  return (res, rej) => {
-    isTextBookOrMovieTitle(taskText)
+  return isTextBookOrMovieTitle(taskText)
       .then(data => {
         console.log('categorizer returns:::', data);
-        return data;
+        switch (data) {
+          case 'watch':
+            return 1;
+            break;
+
+          case 'read':
+            return 2;
+            break;
+
+          case 'eat':
+            return 3;
+            break;
+
+          case 'buy':
+            return 4;
+            break;
+
+        }
+        return 4;
       });
-    };
 };
