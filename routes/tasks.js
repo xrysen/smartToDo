0b0e07696c0bed2d3ae90686d0609cac2e447cb6
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-28 18:49:37 -0800

    Implement POST route for tasks.js with categorizer (currently random)

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,12 +7,13 @@
 
 const express = require('express');
 const router  = express.Router();
-const dbHelper = require('../db/helpers/db_tasks')
+const dbHelper = require('../db/helpers/db_tasks');
+const categorizeTask = require('../externalApis/categorizer');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const userId = req.session.userId;
-    dbHelper.getAllTasksForUser(db, userId)
+    dbHelper.getTasksByUserId(db, userId)
       .then(data => {
         const tasks = data.rows;
         res.json({ tasks });
@@ -24,6 +25,24 @@ module.exports = (db) => {
       });
   });
 
+  router.post("/", (req, res) => {
+    const userId = req.session.userId;
+    const task = req.body // TODO:: check 'task' is the query text
+    console.log('Task req.body:::::::::::\n', req.body);
+
+    const categoryId = categorizeTask(task);
+
+    dbHelper.createNewTask = (db, task, userId, categoryId)
+      .then(data => {
+        const tasks = data.rows;
+        res.json({ tasks });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
 
   return router;
 };
