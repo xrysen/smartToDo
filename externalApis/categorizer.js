a765a876e5052d00849910b6ac6aeb526fb32752
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-30 19:03:42 -0800

    Conduct surprisingly intense linting

--- externalApis/categorizer.js
+++ externalApis/categorizer.js
@@ -12,7 +12,7 @@ const compareStrings = (str1, str2) => {
   if (str1 === str2) return 2;
   if (str1.includes(str2)) return 1;
   return 0;
-}
+};
 
 /**
  * Input:
@@ -26,17 +26,17 @@ const compareStrings = (str1, str2) => {
 const requestBookByTitle = (titleOrAuthor) => {
   // return null
   return request(`https://www.googleapis.com/books/v1/volumes?q=${titleOrAuthor}`)
-  .then(res => {
-    res = JSON.parse(res);
-    if (res.items.length) {
-      console.log('read:', res.items[0].volumeInfo.title);
-      return Math.max(
-        compareStrings(res.items[0].volumeInfo.title.toLowerCase(), titleOrAuthor.toLowerCase()),
-        compareStrings(res.items[0].volumeInfo.authors[0].toLowerCase(), titleOrAuthor.toLowerCase())
-      )
-    }
-    return 0;
-  });
+    .then(res => {
+      res = JSON.parse(res);
+      if (res.items.length) {
+        console.log('read:', res.items[0].volumeInfo.title);
+        return Math.max(
+          compareStrings(res.items[0].volumeInfo.title.toLowerCase(), titleOrAuthor.toLowerCase()),
+          compareStrings(res.items[0].volumeInfo.authors[0].toLowerCase(), titleOrAuthor.toLowerCase())
+        );
+      }
+      return 0;
+    });
 };
 
 /**
@@ -51,14 +51,14 @@ const requestBookByTitle = (titleOrAuthor) => {
 const requestMovieByTitle = (movieTitle) => {
   // return 1
   return request(`http://www.omdbapi.com/?s=${movieTitle}&page=1&apikey=${process.env.API_KEY_OMDB}`)
-  .then(res => {
-    res = JSON.parse(res);
-    if (res.Response === "True" && res.Search.length) {
-      console.log('watch:', res.Search[0].Title);
-      return compareStrings(res.Search[0].Title.toLowerCase(), movieTitle.toLowerCase());
-    }
-    return 0;
-  })
+    .then(res => {
+      res = JSON.parse(res);
+      if (res.Response === "True" && res.Search.length) {
+        console.log('watch:', res.Search[0].Title);
+        return compareStrings(res.Search[0].Title.toLowerCase(), movieTitle.toLowerCase());
+      }
+      return 0;
+    });
 };
 
 /**
@@ -73,20 +73,20 @@ const requestMovieByTitle = (movieTitle) => {
 const requestRestaurantByName = (name) => {
   // return 2
   // Ideally detect user's location via IP address, in this case hardcode
-  const location = 'Vancouver, BC'
+  const location = 'Vancouver, BC';
 
   return request({
     url: `https://api.yelp.com/v3/businesses/search?term=${name}&location=${location}&categories=restaurants&limit=1`,
     headers: { Authorization: `Bearer ${process.env.API_KEY_YELP}` }
   })
-  .then(res => {
-    res = JSON.parse(res);
-    if (res.businesses.length) {
-      console.log('eat:', res.businesses[0].name);
-      return compareStrings(res.businesses[0].name.toLowerCase(), name.toLowerCase());
-    }
-    return 0;
-  });
+    .then(res => {
+      res = JSON.parse(res);
+      if (res.businesses.length) {
+        console.log('eat:', res.businesses[0].name);
+        return compareStrings(res.businesses[0].name.toLowerCase(), name.toLowerCase());
+      }
+      return 0;
+    });
 };
 
 /**
@@ -106,29 +106,29 @@ module.exports = (query) => {
     read: 2,
     eat: 3,
     buy: 4,
-  }
+  };
 
   // Category priority, order of Promise.all() calls must reflect this array
-  const localCategories = ['eat', 'watch', 'read']
+  const localCategories = ['eat', 'watch', 'read'];
 
   return Promise.all([
     requestRestaurantByName(query),
     requestMovieByTitle(query),
     requestBookByTitle(query)
   ])
-  .then(res => {
+    .then(res => {
     // Set default categoryName of 'buy', will be overwritten if any APIs return positive matches
-    let categoryName = 'buy';
-    const maxApiHits = Math.max(...res);
-    if (maxApiHits) {
-      categoryName = localCategories[res.findIndex(x => x === maxApiHits)]
-    }
+      let categoryName = 'buy';
+      const maxApiHits = Math.max(...res);
+      if (maxApiHits) {
+        categoryName = localCategories[res.findIndex(x => x === maxApiHits)];
+      }
 
-    console.log('apiHitsArr::', res)
-    console.log('categoryName:', categoryName);
-    console.log('category_id:', dbCategories[categoryName])
-    return dbCategories[categoryName]
-  })
+      console.log('apiHitsArr::', res);
+      console.log('categoryName:', categoryName);
+      console.log('category_id:', dbCategories[categoryName]);
+      return dbCategories[categoryName];
+    });
 };
 
 
@@ -151,6 +151,7 @@ module.exports = (query) => {
  * eat    cactus club
  * eat    McDonald's
  * eat    tacofino
+ * eat    red fish blue fish
  *
  * buy    new water bottle
  * buy    bluetooth earpods
