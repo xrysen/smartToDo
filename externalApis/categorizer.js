61e2da7bb7fa4477ed5b09213efdbcbabd745895
Adam Tyler <mr.adamtyler@gmail.com>
2020-11-30 17:24:09 -0800

    WIP: remove wolfman beta; change algorithm to prioritize exact match
    - 2 points for exact match, 1 for partial match
    - function currently does not return valuable info, but it tries hard, it's a good dog

--- externalApis/categorizer.js
+++ externalApis/categorizer.js
@@ -1,51 +1,35 @@
 const request = require("request-promise-native");
 
-const requestWolframDatatype = (text) => {
-  return request(`https://api.wolframalpha.com/v2/query?input=${text}&format=plaintext&output=json&appid=${process.env.API_KEY_WOLFRAMALPHA}`)
-  .then(res => {
-    res = JSON.parse(res);
-    console.log('print result.assumptions.values::', res.queryresult.assumptions.values);
-    if (res.queryresult.assumptions) {
-      for (const obj of res.queryresult.assumptions.values) {
-        if (['RetailLocationClass'].includes(obj.name)) {
-          return 'eat';
-        }
-        if (['Movie','MovieClass'].includes(obj.name)) {
-          return 'watch';
-        }
-        if (['Book','BookClass'].includes(obj.name)) {
-          return 'read';
-        }
-      }
-    }
-    return null;
-  })
-};
+const compareStrings = (str1, str2) => {
+  if (str1 === str2) return 2;
+  if (str1.includes(str2)) return 1;
+  return 0;
+}
 
-const requestBookByTitle = (title) => {
+const requestBookByTitle = (bookTitle) => {
   return null
-  return request(`https://www.googleapis.com/books/v1/volumes?q=${title}`)
+  return request(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`)
   .then(res => {
     res = JSON.parse(res);
-    if (res.items[0].volumeInfo.title.toLowerCase().includes(title.toLowerCase())) {
-      return 'read';
-    } else {
-      return null;
+    if (res.items.length) {
+      console.log('read: ', res.items[0].volumeInfo.title);
+      return compareStrings(res.items[0].volumeInfo.title.toLowerCase(), bookTitle.toLowerCase());
     }
+    return 0;
   });
 };
 
 
-const requestMovieByTitle = (title) => {
+const requestMovieByTitle = (movieTitle) => {
   return null
-  return request(`http://www.omdbapi.com/?s=${title}&page=1&apikey=${process.env.API_KEY_OMDB}`)
+  return request(`http://www.omdbapi.com/?s=${movieTitle}&page=1&apikey=${process.env.API_KEY_OMDB}`)
   .then(res => {
     res = JSON.parse(res);
-    if (res.Response === "False") {
-      return null;
-    } else {
-      return 'watch';
-}
+    if (res.Response === "True" && res.Search.length) {
+      console.log('watch: ', res.Search[0].Title);
+      return compareStrings(res.Search[0].Title.toLowerCase(), movieTitle.toLowerCase());
+    }
+    return 0;
   })
 };
 
@@ -61,12 +45,11 @@ const requestRestaurantByName = (name) => {
   })
   .then(res => {
     res = JSON.parse(res);
-    if (!res.businesses.length || !res.businesses[0].name.toLowerCase().includes(name.toLowerCase())) {
-      return null;
+    if (res.businesses.length) {
+      console.log('eat: ', res.businesses[0].name);
+      return compareStrings(res.businesses[0].name.toLowerCase(), name.toLowerCase());
     }
-    else {
-      return 'eat';
-}
+    return 0;
   });
 };
 
@@ -74,55 +57,76 @@ const requestRestaurantByName = (name) => {
  * Input: str
  *  string text to search
  * Output:
- *  sends a request to Wolfram Alpha, Yelp, openMovieDatbase and Google books APIs
- *  returns an array of category names if API has returned a positive result
- *  e.g. [ 'read', 'null', 'watch', 'read' ]
+ *  gathers relevant data from requests to Yelp, openMovieDatbase and Google's books APIs
+ *  returns category_id (matching database) of
+ *   1. category name which occurs twice in array, if any do, or
+ *   2. category name which occurs first in array (Promise.all() order shows preference)
+ *   3. default value of 'buy' if no other categories are found
  */
 const requestAllApis = (str) => {
   return Promise.all([
-    requestWolframDatatype(str),
     requestRestaurantByName(str),
     requestMovieByTitle(str),
     requestBookByTitle(str)
   ])
 };
 
-/**
- * Input:
- *  array of return values from all API calls
- * Output:
- *  category_id (from database) of
- *   1. category name which occurs twice in array, if any do, or
- *   2. category name which occurs first in array (Promise.all() order shows preference)
- *   3. default value of 'buy' if no other categories are found
- */
-const calcCatIdFromApiResults = (apiHitsArr) => {
-  // IMPORTANT: dbCatNames must be in same order as database categories
-  const dbCatNames = ['watch', 'read', 'eat', 'buy'];
-  const countApiHits = [0, 0, 0, 0];
 
-  apiHitsArr.forEach(apiHit => {
-    const i = dbCatNames.findIndex(dbName => dbName === apiHit);
-    if (i !== -1) {
-      countApiHits[i] ++;
+const calcCatIdFromApiResults = (apiHitsArr) => {
+  const dbCategories = {
+    watch: 1,
+    read: 2,
+    eat: 3,
+    buy: 4,
+  }
+
+  const localCategories = ['eat', 'watch', 'read']
+
+  // Set default categoryName of 'buy', overwritten if APIs return hits
+  let categoryName = 'buy';
+  for (const word of apiHitsArr) {
+    if (word) {
+      categoryName = word;
+      break;
     }
-  })
-  console.log('apiHitsArr::', apiHitsArr);
-  console.log('countApiHits::', countApiHits);
-  const maxApiHits = Math.max(...countApiHits);
-  const catIdZeroIndex = countApiHits.findIndex(x => x === maxApiHits);
-  const category_id = catIdZeroIndex + 1
-  return category_id;
+  }
+  // Loop through again, overwrite if exact match exists
+  for (const word of apiHitsArr) {
+    if (Array.isArray(word)) {
+      categoryName = word[0];
+      break;
+    }
+  }
+
+  console.log('apiHitsArr::', apiHitsArr)
+  console.log(categoryName, ':', dbCategories[categoryName])
+  return dbCategories[categoryName]
 }
 
-requestAllApis('socks')
+//---------------------------------------------------
+requestAllApis('starbucks')
 .then(res => {
   return calcCatIdFromApiResults(res);
 })
+//---------------------------------------------------
 
 module.exports = (text) => {
+  const dbCategories = {
+    watch: 1,
+    read: 2,
+    eat: 3,
+    buy: 4,
+  }
+
   return requestAllApis(text)
   .then(res => {
-    return calcCatIdFromApiResults(res);
+    let categoryName = 4;
+    for (const word of res) {
+      if (word) {
+        categoryName = word;
+        break;
+      }
+    }
+    return dbCategories[categoryName]
   })
 };
