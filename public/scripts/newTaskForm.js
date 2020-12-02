1612c06fea2ecfb78f3745f78d1811febbe59ab2
Adam Tyler <mr.adamtyler@gmail.com>
2020-12-01 16:58:21 -0800

    dry newTaskForm

--- public/scripts/newTaskForm.js
+++ public/scripts/newTaskForm.js
@@ -6,18 +6,10 @@ $(() => {
     const $input = $('#todo-text');
     if (error === false) {
       $.ajax(`/api/tasks`, {method: "POST", data: $input.serialize()}) // ajax post request to database,
-        .then(() => { // clears text box
-          $input.val('');
+        .then((res) => {
+          renderListElements(res)
         })
-        .then(() => {
-          $.ajax(`/api/tasks/`, { method: "GET" }) // Refactor to use response from POST
-            .then((res) => {
-              const task = res['tasks'].pop();
-              return task['category_id']
-            })
-            .then((id) => loadListItems(false, id, ));
-        }) // CHANGE WATCH loads new list item HERE is a good point to add JQUERY to make addition really noticable
-        .fail((err) => console.log(err));
+        .catch((err) => console.log(err));
     }
   });
 
