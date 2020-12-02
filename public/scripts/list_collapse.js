d7825a29fd9b31c2d2cf27b8ba65ceb85eec2fc2
remy29 <arthur.d.remy@gmail.com>
2020-12-02 17:51:48 +0000

    minor revisions

--- public/scripts/list_collapse.js
+++ public/scripts/list_collapse.js
@@ -29,6 +29,29 @@ $(document).ready(function() {
     $("#to-buy").slideToggle();
   });
 
-
+/*   const hideTaskOnUserActive = function () {
+    $.ajax(`/api/users/active`, { method: 'GET' })
+      .then((res) => {
+        isUserActive(res);
+      })
+  }
+  const hideUnpopulatedLists = function (category) {
+    $.ajax(`/api/tasks/getByCategory/${category}`, { method: 'GET' })
+    .then((res) => {
+      console.log(res.tasks)
+      console.log(res.tasks.length)
+      if (!res.tasks[0]) {
+        return false;
+      }
+    })
+    .then((res) => {
+      if (res === false) {
+        $(`.${category}`).hide();
+      }
+    })
+  }
+  for (let i = 1; i < 5; i++) {
+    hideUnpopulatedLists(i);
+  } */
 });
 
