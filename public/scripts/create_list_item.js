cd554ebfc8323ae5efd0c834be00dd9f36d0554a
Sean Oyler <xrysen@gmail.com>
2020-12-03 02:15:24 +0000

    Empty lists will now disappear on drag and drop

--- public/scripts/create_list_item.js
+++ public/scripts/create_list_item.js
@@ -82,23 +82,25 @@ $(() => {
       {
         drop: function(ev, ui) {
           const dropped = ui.draggable.attr("id");
+          let oldCat = ui.draggable.attr("class");
+
           const taskId = dropped.substring(dropped.indexOf('-') + 1);
           switch($(this).attr("id")) {
             case "4-table":
               console.log("Moving to category 4");
-              moveTask(taskId, $taskCatId, 4);
+              moveTask(taskId, oldCat[12], 4);
               break;
             case "3-table":
               console.log("Moving to category 3");
-              moveTask(taskId, $taskCatId, 3);
+              moveTask(taskId, oldCat[12], 3);
               break;
             case "2-table":
               console.log("Moving to category 2");
-              moveTask(taskId, $taskCatId, 2);
+              moveTask(taskId, oldCat[12], 2);
               break;
             case "1-table":
               console.log("Moving to category 1");
-              moveTask(taskId, $taskCatId, 1);
+              moveTask(taskId, oldCat[12], 1);
               break;
           }
         }
